75bc8ac76d44504d4dd93c11a1d7fe5d8b16959a
Rodrigo Martins <rodrigomartins_santos1804@hotmail.com>
2020-07-29 16:49:04 -0300

    added Sass and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom' 
 
 const App = () => {
-  return <h1>Link</h1>
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li> <Link to="/sign-in"> Sign in</Link> </li>
+            <li> <Link to="/sign-up"> Sign up</Link> </li>
+          </ul>
+        </nav>
+        <Switch>
+          
+          <Route path="/sign-in">
+            <h1>SignIn</h1>
+          </Route>
+
+          <Route path="/sign-up">
+            <h1>Signup</h1>
+          </Route>
+
+        </Switch>
+      </div>
+    
+    
+    </BrowserRouter>
+  )
 }
 
 export default App;
