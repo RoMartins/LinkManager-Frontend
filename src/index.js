a2cc2a3ab1bf140238bcbeb3fb7834ff81550d07
Rodrigo Martins <rodrigomartins_santos1804@hotmail.com>
2020-07-29 18:00:59 -0300

    added UI ki

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDom from  'react-dom';
 
 import App from './App'
-import './main.scss'
+import './Styles/main.scss'
 
 ReactDom.render(<App />, document.getElementById('root'));
